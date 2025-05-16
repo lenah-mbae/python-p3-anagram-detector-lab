4f81cb535f86fb186511acf69c28616eb9206020
Lenah Mbae <lenahmbae22@gmail.com>
2025-05-16 18:54:09 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,27 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted (self.word)
+    
+    def match(self, character_list):
+        return[
+            character for character in character_list
+            if character.lower() != self.word and sorted(character.lower()) == self.sorted_word
+        ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
